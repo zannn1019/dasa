137f2e99a738a76c707baa9977ca7bbee34e41d4
zannn1019 <fauzanguci1019@gmail.com>
2026-03-26 15:20:03 +0700

    feat: Configure Strapi security middleware with Content Security Policy and add a new origin to the CORS middleware.

--- dasa-cms/config/middlewares.ts
+++ dasa-cms/config/middlewares.ts
@@ -3,11 +3,29 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
-      origin: ['https://n8n.talentaraya.co.id', 'http://localhost:3000', 'http://localhost:3001'],
+      origin: [
+        'https://cms.talentaraya.co.id',
+        'https://n8n.talentaraya.co.id',
+        'http://localhost:3000',
+        'http://localhost:3001'
+      ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
